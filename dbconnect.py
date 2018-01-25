59c616b5fe9a742d706e1a96ad43071e840b1dc2
Mikhail <m.v.bobrov@yandex.ru>
2018-01-25 21:06:50 +0300

    some update

--- dbconnect.py
+++ dbconnect.py
@@ -1,10 +1,10 @@
 import urllib.parse as urlparse
 from pg import DB
 
+
 class LMSDB:
     def __init__(self, db_url):
         urlparse.uses_netloc.append("postgres")
-        self.__db_url = db_url
         url = urlparse.urlparse(db_url)
         self.__db = DB(
             dbname=url.path[1:],
@@ -13,3 +13,6 @@ class LMSDB:
             host=url.hostname,
             port=url.port
         )
+
+    def close(self):
+        self.__db.close()
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 from dbconnect import LMSDB
 
 db = LMSDB("postgres://admin:AdminsStrongPassword@localhost:5432/LynxLMS")
+db.close()
