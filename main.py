3ad3d19d34d76ea7853b6a22b78d601fc0bb43ea
Gayaz Kamaletdinov <gazikkamalov2@gmail.com>
2018-01-27 19:16:35 +0300

    changed style

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QStyleFactory
 
 import MainWindow
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    QApplication.setStyle(QStyleFactory.create('Fusion'))
 
     main_window = MainWindow.MainWindow()
     main_window.show()
