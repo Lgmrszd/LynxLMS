f2a568cf2498189799a9b914bc3ddad7709c813c
Gayaz Kamaletdinov <gazikkamalov2@gmail.com>
2018-01-28 17:22:53 +0300

    table in SearchWindow, new BookInfo class

---
+++ BookInfo.py
@@ -0,0 +1,64 @@
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel, QGroupBox
+
+
+class BookInfo(QWidget):
+    def __init__(self):
+        super().__init__()
+        self._set_up_ui()
+
+    def _set_up_ui(self):
+        window_size_x = 400
+        window_size_y = 250
+
+        self.book_name = QLabel("<h1>The art of QLabel</h1>")
+        self.book_name.setFixedWidth(window_size_x - 20)
+        self.book_name.setWordWrap(True)
+
+        self.book_author = QLabel("<h2>unknown</h2>")
+        self.book_author.setFixedWidth(window_size_x - 20)
+        self.book_author.setWordWrap(True)
+
+        self.book_description = QLabel("<h4>In order to show multiple lines in QLabel, right click on QLabel and select 'change rich text'. This brings up dialog where you can type the text as you want to see including enter key. Setting the word wrap is not required for this.If you set the word wrap as well (in QLabel properties) than it will wrap each individual line in the Qlabel if it was longer than the real estate.</h4>")
+        self.book_description.setFixedWidth(window_size_x - 20)
+        self.book_description.setWordWrap(True)
+
+        book_genres = [QLabel("genre 1"), QLabel("genre 2")]
+
+        name_box = QGroupBox()
+        name_box.setTitle("Book name")
+        name_layout = QVBoxLayout()
+        name_layout.addWidget(self.book_name)
+        name_layout.addStretch(1)
+        name_box.setLayout(name_layout)
+
+        author_box = QGroupBox()
+        author_box.setTitle("Author")
+        author_layout = QVBoxLayout()
+        author_layout.addWidget(self.book_author)
+        author_layout.addStretch(1)
+        author_box.setLayout(author_layout)
+
+        description_box = QGroupBox()
+        description_box.setTitle("Book description")
+        description_layout = QVBoxLayout()
+        description_layout.addWidget(self.book_description)
+        description_layout.addStretch(1)
+        description_box.setLayout(description_layout)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(name_box)
+        vbox.addWidget(author_box)
+        vbox.addWidget(description_box)
+
+        self.setLayout(vbox)
+        self.resize(window_size_x, window_size_y)
+        self.setWindowTitle('Book information')
+
+    def set_book_name(self, book_name):
+        self.book_name.setText(book_name)
+
+    def set_book_author(self, book_author):
+        self.book_author.setText(book_author)
+
+    def set_book_description(self, book_description):
+        self.book_description.setText(book_description)
--- SearchWindow.py
+++ SearchWindow.py
@@ -1,5 +1,7 @@
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel, QGroupBox, QLineEdit, QPushButton
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel, QGroupBox, QLineEdit, QPushButton, QTableWidget,\
+    QTableWidgetItem, QAbstractItemView
 
+from BookInfo import BookInfo
 
 class SearchWindow(QWidget):
     def __init__(self, parent=None):
@@ -10,15 +12,38 @@ class SearchWindow(QWidget):
     def closeEvent(self, event):
         self.par.show()
 
+    def set_up_table(self, table):
+        self.result_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.result_table.setColumnCount(3)
+        self.result_table.setRowCount(20)
+
+        ID_item = QTableWidgetItem("ID")
+        self.result_table.setHorizontalHeaderItem(0, ID_item)
+
+        book_name_item = QTableWidgetItem("Book name")
+        self.result_table.setHorizontalHeaderItem(1, book_name_item)
+
+        author_item = QTableWidgetItem("Author")
+        self.result_table.setHorizontalHeaderItem(2, author_item)
+
+        self.result_table.setColumnWidth(0, 80)
+        self.result_table.setColumnWidth(1, 380)
+        self.result_table.setColumnWidth(2, 250)
+
     def _set_up_ui(self):
         window_size_x = 800
         window_size_y = 600
+
+        self.books = []
 
         self.search_field = QLineEdit("")
         additional_options_button = QPushButton("...")
 
         result_group = QGroupBox("")
-        self.test_label = QLabel("kek322")
+
+        self.result_table = QTableWidget()
+        self.set_up_table(self.result_table)
+        self.result_table.doubleClicked.connect(self.cell_clicked_event)
 
         search_button = QPushButton("Search")
         search_button.clicked.connect(self.get_result)
@@ -28,7 +53,7 @@ class SearchWindow(QWidget):
         search_layout.addWidget(additional_options_button)
 
         in_group_layout = QVBoxLayout()
-        in_group_layout.addWidget(self.test_label)
+        in_group_layout.addWidget(self.result_table)
         result_group.setLayout(in_group_layout)
 
         search_button_layout = QHBoxLayout()
@@ -42,8 +67,16 @@ class SearchWindow(QWidget):
 
         self.setLayout(full_layout)
 
-        self.setFixedSize(window_size_x, window_size_y)
+        self.resize(window_size_x, window_size_y)
         self.setWindowTitle('Search')
 
+    def cell_clicked_event(self, event):
+        print(str(event.row()) + " row clicked")
+        book_info_window = BookInfo()
+        book_info_window.set_book_author("me")
+        book_info_window.show()
+        self.books.append(book_info_window)  #to prevent deletion of book_info_window, because book_info_window is local variable
+
+
     def get_result(self):#вызывается при нажатии на кнопку 'search', вызвав 'self.search_field.text()' можно получить тескт из строки поиска
-        self.test_label.setText(self.test_label.text() + self.search_field.text())
+        print('\'' + self.search_field.text() + '\' searched')
