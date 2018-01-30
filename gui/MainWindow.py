3c63daeb43e056925cacaf05ad051b84092668df
Gayaz Kamaletdinov <gazikkamalov2@gmail.com>
2018-01-30 21:53:19 +0300

    addBook class

--- gui/MainWindow.py
+++ gui/MainWindow.py
@@ -1,12 +1,13 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QDesktopWidget, QPushButton, QVBoxLayout
 from gui.SearchWindow import SearchWindow
-
+from gui.AddBook import AddBook
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.search_window = SearchWindow(self)
+        self.add_books = []
         self._set_up_ui()
 
     def closeEvent(self, QCloseEvent):#вызывается при close event
@@ -19,12 +20,18 @@ class MainWindow(QWidget):
         search_button = QPushButton("Search books")
         search_button.setFixedHeight(30)
         search_button.clicked.connect(self.switch_search_window)
+
         manage_users_button = QPushButton("Manage users")
         manage_users_button.setFixedHeight(30)
+
+        add_book_button = QPushButton("Add book")
+        add_book_button.setFixedHeight(30)
+        add_book_button.clicked.connect(self.open_add_book_window)
 
         vbox = QVBoxLayout()
         vbox.setSpacing(10)
         vbox.addWidget(search_button)
+        vbox.addWidget(add_book_button)
         vbox.addWidget(manage_users_button)
         vbox.addStretch()
 
@@ -45,3 +52,8 @@ class MainWindow(QWidget):
             self.search_window.show()
         else:
             self.search_window.hide()
+
+    def open_add_book_window(self):
+        add_book = AddBook()
+        add_book.show()
+        self.add_books.append(add_book)
