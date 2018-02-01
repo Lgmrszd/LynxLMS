2cb3de71d7cd3d95fe34ce45244503db23748ebb
PolarNorth <35637600+PolarNorth@users.noreply.github.com>
2018-02-01 16:31:06 +0300

    Added modification methods to document manager
    Added modification methods to document manager, updated db_start, added database.db

--- managers/doc_manager.py
+++ managers/doc_manager.py
@@ -1,18 +1,41 @@
 import peewee as pw
 import json
+import sys
+import inspect
 from dbconfig import BaseModel
-
 
 class Document(BaseModel):
     DocumentID = pw.PrimaryKeyField()
-    typeID = pw.IntegerField()
+    #typeID = pw.IntegerField()
     title = pw.CharField()
     author = pw.CharField()
     cost = pw.IntegerField()
     keywords = pw.CharField()
 
     @classmethod
+    def add(cls, args):
+        """Creates a new entity of selected type. Takes on input dictionary of values"""
+        cls.create(**args)
+
+    @classmethod
+    def remove(cls, doc_id):
+        """Removes an entity with specific DocumentID"""
+        temp = cls.get(DocumentID = doc_id)
+        print(temp.title)
+        temp.delete_instance()
+    
+    @classmethod
+    def edit(cls, doc_id, new_values):
+        """Edit certain values in an entity with specific DocumentID"""
+        temp = cls.get(DocumentID = doc_id)
+        for k in new_values.keys():
+            temp.__dict__['_data'][k] = new_values[k]
+        temp.save()
+        print(temp.__dict__)
+
+    @classmethod
     def get_fields(cls):
+        """Returns list of fields of specific document type"""
         temp = {**cls.__dict__ , **Document.__dict__}
         temp.pop('__doc__')
         temp.pop('__module__')
@@ -31,3 +54,8 @@ class Journal_article(Document):
 class AV_material(Document):
     pass
 
+#clsmembers = inspect.getmembers(sys.modules[__name__], inspect.isclass)
+#for x in clsmembers:
+#    if x[0] == 'BaseModel':
+#        continue
+#    Document._meta.database.create_table(x[1])
