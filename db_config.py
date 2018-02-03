4d18978299f041fa17828a03f2e80408dae9fbe9
PolarNorth <35637600+PolarNorth@users.noreply.github.com>
2018-02-03 18:15:07 +0300

    Added tables Copy and History
    Added tables Copy and History

--- db_config.py
+++ db_config.py
@@ -1,6 +1,7 @@
 import managers.doc_manager
 import managers.user_manager
 import managers.group_manager
+import managers.booking_system
 from db_connect import db
 
 
@@ -9,13 +10,17 @@ def initialize_db():
     db.create_tables([managers.doc_manager.Book,
                       managers.doc_manager.AVMaterial,
                       managers.doc_manager.JournalArticle,
+                      managers.doc_manager.Copy,
                       managers.user_manager.User,
-                      managers.group_manager.Group], safe=True)
+                      managers.group_manager.Group,
+                      managers.booking_system.History], safe=True)
 
 
 def drop_db():
     db.drop_tables([managers.doc_manager.Book,
                     managers.doc_manager.AVMaterial,
                     managers.doc_manager.JournalArticle,
+                    managers.doc_manager.Copy,
                     managers.user_manager.User,
-                    managers.group_manager.Group], safe=True)
+                    managers.group_manager.Group,
+                    managers.booking_system.History], safe=True)
