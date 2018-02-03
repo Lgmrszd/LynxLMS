960fe3c9b837a5ace1f3809ef4e2f6c489adcd4e
Gayaz Kamaletdinov <gazikkamalov2@gmail.com>
2018-02-03 20:56:45 +0300

    searchSettings [?] button deleted, ok button added.

--- gui/SearchSettings.py
+++ gui/SearchSettings.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel, QGroupBox, QLineEdit, QPushButton, QTableWidget,\
     QTableWidgetItem, QAbstractItemView, QComboBox, QTextEdit, QCheckBox, QScrollArea, QInputDialog, QDialog
+from PyQt5.QtCore import Qt
 
 
 class SearchSettings(QDialog):
@@ -10,6 +11,8 @@ class SearchSettings(QDialog):
         window_size_y = 240
         self.resize(window_size_x, window_size_y)
 
+        self.setWindowFlags(self.windowFlags() & ~Qt.WindowContextHelpButtonHint)
+
         sort_label = QLabel("Sort by:")
         self.sortBox = QComboBox()
         self.sortBox.addItems(["Popular", "Name", "New"])
@@ -18,12 +21,19 @@ class SearchSettings(QDialog):
         self.typeBox = QComboBox()
         self.typeBox.addItems(["All", "Book", "Journal", "AV"])
 
+        ok_button = QPushButton("OK")
+        ok_button.clicked.connect(self.close)
+
         vbox = QVBoxLayout()
         vbox.addWidget(sort_label)
         vbox.addWidget(self.sortBox)
         vbox.addWidget(type_label)
         vbox.addWidget(self.typeBox)
         vbox.addStretch()
+        ok_layout = QHBoxLayout()
+        ok_layout.addStretch()
+        ok_layout.addWidget(ok_button)
+        vbox.addLayout(ok_layout)
 
         self.setLayout(vbox)
         self.setWindowTitle("Search settings")
