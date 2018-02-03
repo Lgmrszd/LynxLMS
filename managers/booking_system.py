f97462ae25a236c3c2e9bc52468c911b232b327d
PolarNorth <35637600+PolarNorth@users.noreply.github.com>
2018-02-03 19:43:17 +0300

    Bug fixes and changes for new Copy class
    Fixed bug with History.librarian_re and made changes in check out and return methods due to changes in Copy class

--- managers/booking_system.py
+++ managers/booking_system.py
@@ -11,20 +11,27 @@ class History(BaseModel):
     copy = pw.ForeignKeyField(doc_manager.Copy, related_name = 'operations')
     librarian_co = pw.CharField()
     date_check_out = pw.DateField(formats = '%Y-%m-%d')
-    librarian_re = pw.CharField()
+    librarian_re = pw.CharField(null = True)
     date_return = pw.DateField(formats = '%Y-%m-%d', null = True)
 
 
 class Booking_system:
     def check_out(self, user, copy, librarian):
+        if copy.checked_out == True:
+            return 0
         current_date = datetime.date.today()
-        History.create(user = user, copy = copy, librarian_co = librarian, date_check_out = current_date)
+        res = History.create(user = user, copy = copy, librarian_co = librarian, date_check_out = current_date)
+        copy.checked_out = True
+        copy.save()
+        return res
     
     def return_by_entry(self, entry, librarian):
         current_date = datetime.date.today()
         entry.date_return = str(current_date)
         entry.librarian_re = librarian
         entry.save()
+        entry.copy.checked_out = False
+        entry.copy.save()
     
     def return_by_copy(self, copy, librarian):
         current_date = datetime.date.today()
@@ -32,6 +39,9 @@ class Booking_system:
         entry.date_return = str(current_date)
         entry.librarian_re = librarian
         entry.save()
+        copy.checked_out = False
+        copy.save()
+
     
     def get_list_overdue(self):
         query = History.select().where(History.date_return.is_null(True))
