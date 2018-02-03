fc03eafcd9265d429ded7d3d2d37e06f679d9455
Mikhail <m.v.bobrov@yandex.ru>
2018-02-03 12:51:09 +0300

    moved db_start to dbconfig

--- dbconfig.py
+++ dbconfig.py
@@ -1,4 +1,7 @@
 from peewee import *
+import managers.doc_manager
+import managers.user_manager
+import managers.group_manager
 
 db = SqliteDatabase('data/database.db')
 
@@ -6,3 +9,25 @@ db = SqliteDatabase('data/database.db')
 class BaseModel(Model):
     class Meta:
         database = db
+
+
+def initialize_db():
+    # Connecting database
+    db.connect()
+
+    # Creating tables
+    db.create_tables([managers.doc_manager.Book,
+        managers.doc_manager.AV_material,
+        managers.doc_manager.Journal_article,
+        managers.user_manager.User,
+        managers.group_manager.Group], safe=True)
+
+
+def drop_db():
+    db.connect()
+
+    db.drop_tables([managers.doc_manager.Book,
+        managers.doc_manager.AV_material,
+        managers.doc_manager.Journal_article,
+        managers.user_manager.User,
+        managers.group_manager.Group], safe=True)
