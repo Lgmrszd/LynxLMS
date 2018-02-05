d8561196fc33fe92826678a9fa91cc02e2f46a5f
Mikhail <m.v.bobrov@yandex.ru>
2018-02-05 20:14:29 +0300

    custom db file

--- db_connect.py
+++ db_connect.py
@@ -3,6 +3,11 @@ from peewee import *
 db = SqliteDatabase('data/database.db')
 db.connect()
 
+
 class BaseModel(Model):
     class Meta:
         database = db
+
+
+def custom_db(dbfile):
+    db.init(dbfile)
