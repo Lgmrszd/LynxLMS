9e2dbfd96df4b877a0ac3c78d2a43afaa0e3c9c4
PolarNorth <antonov9312@yandex.ru>
2018-02-06 21:16:59 +0300

    Documentation for booking system

--- managers/booking_system.py
+++ managers/booking_system.py
@@ -6,6 +6,8 @@ import datetime
 
 
 class History(BaseModel):
+    """Data model for all checkout and return operations
+    """
     OperationID = pw.PrimaryKeyField()
     user = pw.ForeignKeyField(user_manager.User, related_name = 'operations')
     copy = pw.ForeignKeyField(doc_manager.Copy, related_name = 'operations')
@@ -16,9 +18,13 @@ class History(BaseModel):
 
 
 class Booking_system:
+    """Booking system class
+    """
     __fine = 100
 
     def check_out(self, user, copy, librarian):
+        """Check out copy of specific document to specific user
+        """
         if 'reference' in copy.get_doc().keywords:
             return 0
         if copy.checked_out == True:
@@ -30,6 +36,8 @@ class Booking_system:
         return res
     
     def return_by_entry(self, entry, librarian):
+        """Return copy by "History" entry
+        """
         current_date = datetime.date.today()
         entry.date_return = str(current_date)
         entry.librarian_re = librarian
@@ -41,11 +49,15 @@ class Booking_system:
         
     
     def return_by_copy(self, copy, librarian):
+        """Return copy
+        """
         entry = History.select().where(History.date_return.is_null(True) & History.copy == copy).get()
         self.return_by_entry(entry, librarian)
 
     
     def get_list_overdue(self):
+        """Get list of overdue items
+        """
         opened = self.get_list_opened()
         res = []
         for entry in opened:
@@ -54,6 +66,8 @@ class Booking_system:
         return res
 
     def get_list_opened(self):
+        """Get list of items that are checked out but not returned yet
+        """
         query = History.select().where(History.date_return.is_null(True))
         res = []
         for entry in query:
@@ -61,6 +75,8 @@ class Booking_system:
         return res
 
     def get_user_history(self, user):
+        """Get all operation for particular user
+        """
         query = user.operations
         res = []
         for entry in query:
@@ -68,6 +84,8 @@ class Booking_system:
         return res
     
     def get_copy_history(self, copy):
+        """Get all operations for particular copy
+        """
         query = copy.operations
         res = []
         for entry in query:
@@ -75,15 +93,18 @@ class Booking_system:
         return res
     
     def get_document_copies(self, doc):
+        """Get list of copies of speciific document
+        """
         doc_class = doc_manager.class_to_name()[type(doc)]
         query = doc_manager.Copy.select().where(doc_manager.Copy.docClass == doc_class , doc_manager.Copy.docId == doc.DocumentID)
-        #print('::: doc_class = ' + doc_class + ' ::: docId = ' + str(doc.DocumentID) + '\n')
         res = [] 
         for entry in query:
             res.append(entry)
         return res
 
     def check_overdue(self, entry):
+        """Returns fine for overdue (0 if no fine)
+        """
         period = 7 * 2
         if (type(entry.copy.get_doc()) == doc_manager.name_to_class()['Book'] & 'best seller' in entry.copy.get_doc().keywords):
             period = entry.user.group.book_bestseller_ct * 7
@@ -93,6 +114,8 @@ class Booking_system:
         return res
 
     def overdue(self, date_check_out, date_return, period):
+        """Calculates fine
+        """
         d1_l = date_return.split('-')
         d2_l = date_check_out.split('-')
         d1 = datetime.date(int(d1_l[0]), int(d1_l[1]), int(d1_l[2]))
@@ -101,5 +124,8 @@ class Booking_system:
         return res
     
     def pay_fine(self, usr, amount):
+        """Pay fine of specific user.
+        Returns change
+        """
         usr.fine = max(usr.fine - amount, 0)
         return (max(amount - usr.fine, 0))
