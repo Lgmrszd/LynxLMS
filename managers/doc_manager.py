8bc8c5f7523ed19457d6afd7d753716968210be7
PolarNorth <antonov9312@yandex.ru>
2018-02-06 21:40:50 +0300

    Documentation for document manager

--- managers/doc_manager.py
+++ managers/doc_manager.py
@@ -5,6 +5,8 @@ import inspect
 
 
 class Document(BaseModel):
+    """Base data model for all document classes
+    """
     DocumentID = pw.PrimaryKeyField()
     title = pw.CharField()
     author = pw.CharField()
@@ -15,23 +17,28 @@ class Document(BaseModel):
 
     @classmethod
     def get_by_id(cls, doc_id):
+        """Get document by id
+        """
         return cls.get(DocumentID = doc_id)
     
     @classmethod
     def add(cls, args):
-        """Creates a new entity of selected type. Takes on input dictionary of values"""
+        """Creates a new entity of selected type. Takes on input dictionary of values
+        """
         return cls.create(**args)
 
     @classmethod
     def remove(cls, doc_id):
-        """Removes an entity with specific DocumentID"""
+        """Removes an entity with specific DocumentID
+        """
         temp = cls.get(DocumentID=doc_id)
         print(temp.title)
         temp.delete_instance()
     
     @classmethod
     def edit(cls, doc_id, new_values):
-        """Edit certain values in an entity with specific DocumentID"""
+        """Edit certain values in an entity with specific DocumentID
+        """
         temp = cls.get(DocumentID=doc_id)
         for k in new_values.keys():
             temp.__dict__['_data'][k] = new_values[k]
@@ -40,7 +47,8 @@ class Document(BaseModel):
 
     @classmethod
     def get_list(cls, rows_number, page):
-        """Returns a content from certain page of document list"""
+        """Returns a content from certain page of document list
+        """
         query = cls.select().offset(0 + (page-1)*rows_number).limit(rows_number).order_by(cls.title.asc())
         res = []
         for entry in query:
@@ -49,7 +57,8 @@ class Document(BaseModel):
 
     @classmethod
     def get_fields(cls):
-        """Returns list of fields of specific document type"""
+        """Returns list of fields of specific document type
+        """
         temp = {**Document.__dict__, **cls.__dict__}
         temp.pop('__doc__')
         temp.pop('__module__')
@@ -61,7 +70,8 @@ class Document(BaseModel):
     
     @classmethod
     def get_fields_dict(cls):
-        """Returns dictionary with field name as a key and type of field as a value"""
+        """Returns dictionary with field name as a key and type of field as a value
+        """
         map_to_types = {
             pw.IntegerField : int,
             pw.CharField : str,
@@ -86,22 +96,30 @@ class Document(BaseModel):
 
 
 class Book(Document):
+    """Data model for Book
+    """
     edition = pw.CharField()
     publisher = pw.CharField()
     year = pw.IntegerField()
 
 
 class JournalArticle(Document):
+    """Data model for Journal Article
+    """
     journal = pw.CharField()
     issue = pw.CharField()
     editor = pw.CharField()
 
 
 class AVMaterial(Document):
+    """Data model for Audio Video Material
+    """
     pass
 
 
 class Copy(BaseModel):
+    """Data model for Copy
+    """
     CopyID = pw.PrimaryKeyField()
     #DocReference = pw.ForeignKeyField(Document, related_name = 'copies')
     docClass = pw.CharField()
@@ -110,21 +128,29 @@ class Copy(BaseModel):
     storage = pw.CharField(default='')
 
     def get_doc(self):
+        """Get the document to which this copy referred
+        """
         doc_class = name_to_class()[self.docClass]
         return doc_class.get_by_id(self.docId)
     
     @classmethod
     def add(cls, doc):
+        """Add copy of specific document
+        """
         return cls.create(docClass = class_to_name()[type(doc)], docId = doc.DocumentID)
     
     @classmethod
     def edit_storage(cls, copy_id, new_storage):
+        """Edit storage place for specific copy
+        """
         temp = cls.get(CopyID = copy_id)
         temp.storage = new_storage
         temp.save()
 
 
 def name_to_class():
+    """Returns a map that has names of classes as keys and classes as values
+    """
     classes = inspect.getmembers(sys.modules[__name__], inspect.isclass)
     res = {}
     for c in classes:
@@ -133,6 +159,8 @@ def name_to_class():
 
 
 def class_to_name():
+    """Returns a map that has classes as keys and class names as values
+    """
     classes = inspect.getmembers(sys.modules[__name__], inspect.isclass)
     res = {}
     for c in classes:
