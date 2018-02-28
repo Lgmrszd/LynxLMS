444492c6e1a2d4e3deacb43e9f0276bf659d08c8
PolarNorth <antonov9312@yandex.ru>
2018-02-28 22:14:02 +0300

    Create "Deleted" group on initialization

--- db_config.py
+++ db_config.py
@@ -14,7 +14,10 @@ def initialize_db():
                       managers.user_manager.User,
                       managers.group_manager.Group,
                       managers.booking_system.History], safe=True)
-
+    #deleted_group = managers.group_manager.Group.get(managers.group_manager.Group.name == 'Deleted')
+    deleted_group = len(managers.group_manager.Group.select().where(managers.group_manager.Group.name == 'Deleted'))
+    if (deleted_group == 0):
+        managers.group_manager.Group.create(name='Deleted', book_ct=-1, book_bestseller_ct=-1, journal_ct=-1, av_ct=-1)
 
 def drop_db():
     db.drop_tables([managers.doc_manager.Book,
