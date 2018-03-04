982ab04416453e2b3b6e724e25262b737aaebef7
Mikhail <m.v.bobrov@yandex.ru>
2018-03-04 14:08:07 +0300

    Test's documentation

--- tests/test_cases_1.py
+++ tests/test_cases_1.py
@@ -18,6 +18,7 @@ def prepare_database():
 class TestCases1(unittest.TestCase):
     def test_case_1(self):
         prepare_database()
+        # Sample group
         group_1 = group_manager.Group.add(
             {
                 "name":"Group",
@@ -27,6 +28,7 @@ class TestCases1(unittest.TestCase):
                 "av_ct":2
             }
         )
+        # Sample user
         user_1 = user_manager.User.add(
             {
                 "name":"User",
@@ -36,6 +38,7 @@ class TestCases1(unittest.TestCase):
                 "group":group_1
             }
         )
+        # Sample book
         book_1 = doc_manager.Book.add(
             {
                 "title":"title",
@@ -47,18 +50,26 @@ class TestCases1(unittest.TestCase):
                 "year":2000
             }
         )
+        # Book's copies
         b1_copy1 = doc_manager.Copy.add(book_1)
         b1_copy2 = doc_manager.Copy.add(book_1)
 
+        # Checkout first copy to a user
         bsystem = booking_system.Booking_system()
         bsystem.check_out(user_1, b1_copy1, "Librarian")
 
+        # Get copies and make sure there are only two of them
         b1_copies = book_1.get_document_copies()
         self.assertTrue(len(b1_copies) == 2)
+
         b1_copy1_after = b1_copies[0]
         b1_copy2_after = b1_copies[1]
+
+        # Make sure that user's story contains only one entry
         user1_history = bsystem.get_user_history(user_1)
         self.assertTrue(len(user1_history) == 1)
+
+        # Make sure that copy given to user and this is one of the existing copies
         uh = user1_history[0]
         self.assertTrue(uh.user.card_id == user_1.card_id)
         self.assertTrue((b1_copy1_after.CopyID == uh.copy.CopyID) or (b1_copy2_after.CopyID == uh.copy.CopyID))
