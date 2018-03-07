9b841e61b2ec3f3afaf15b4b27f07fbfa9c536fe
Mikhail <m.v.bobrov@yandex.ru>
2018-03-07 15:22:48 +0300

    test cases 2

---
+++ tests/test_cases_2.py
@@ -0,0 +1,286 @@
+import unittest
+import managers.doc_manager as doc_manager
+import managers.booking_system as booking_system
+import managers.user_manager as user_manager
+import managers.group_manager as group_manager
+import db_config
+import datetime
+
+
+class TestCases2(unittest.TestCase):
+    def prepare_database(self):
+        db_fname = "test_database.db"
+        db_config.init_db(db_fname)
+        db_config.drop_tables()
+        db_config.create_tables()
+
+        self.g_f = group_manager.Group.add(
+            {
+                "name": "Faculty",
+                "book_ct": 4,
+                "book_bestseller_ct": 4,
+                "journal_ct": 2,
+                "av_ct": 2
+            }
+        )
+        self.g_s = group_manager.Group.add(
+            {
+                "name": "Students",
+                "book_ct": 3,
+                "book_bestseller_ct": 2,
+                "journal_ct": 2,
+                "av_ct": 2
+            }
+        )
+
+    def test_case_1(self):
+        self.prepare_database()
+
+        self.b1 = doc_manager.Book.add(
+            {
+                "title": "Introduction to Algorithms",
+                "author": "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest and Clifford Stein",
+                "cost": 300,
+                "keywords": "",
+                "edition": "Third edition",
+                "publisher": "MIT Press",
+                "year": 2009
+            }
+        )
+        self.b2 = doc_manager.Book.add(
+            {
+                "title": "Design Patterns: Elements of Reusable Object-Oriented Software",
+                "author": "Erich Gamma, Ralph Johnson, John Vlissides, Richard Helm",
+                "cost": 300,
+                "keywords": "best seller",
+                "edition": "First edition",
+                "publisher": "Addison-Wesley Professional",
+                "year": 2003
+            }
+        )
+        self.b3 = doc_manager.Book.add(
+            {
+                "title": "The Mythical Man-month",
+                "author": "Brooks,Jr., Frederick P.",
+                "cost": 300,
+                "keywords": "reference",
+                "edition": "Second edition",
+                "publisher": "Addison-Wesley Professional",
+                "year": 1995
+            }
+        )
+
+        self.b1c1 = doc_manager.Copy.add(self.b1)
+        self.b1c2 = doc_manager.Copy.add(self.b1)
+        self.b1c3 = doc_manager.Copy.add(self.b1)
+        self.b2c1 = doc_manager.Copy.add(self.b2)
+        self.b2c2 = doc_manager.Copy.add(self.b2)
+        self.b3c1 = doc_manager.Copy.add(self.b3)
+
+        self.av1 = doc_manager.AVMaterial.add(
+            {
+                "title": "Null References: The Billion Dollar Mistake",
+                "author": "Tony Hoare",
+                "cost": 1000000000,
+                "keywords": "",
+            }
+        )
+
+        self.av2 = doc_manager.AVMaterial.add(
+            {
+                "title": "Information Entropy",
+                "author": "Claude Shannon",
+                "cost": 100,
+                "keywords": "",
+            }
+        )
+
+        self.av1c1 = doc_manager.Copy.add(self.av1)
+        self.av2c1 = doc_manager.Copy.add(self.av2)
+
+        self.p1 = user_manager.User.add(
+            {
+                "name": "Sergey",
+                "surname": "Afonso",
+                "address": "Via Margutta, 3",
+                "phone": 30001,
+                "group": self.g_f
+            }
+        )
+        self.p2 = user_manager.User.add(
+            {
+                "name": "Nadia",
+                "surname": "Teixeira",
+                "address": "Via Sacra, 13",
+                "phone": 30002,
+                "group": self.g_s
+            }
+        )
+        self.p3 = user_manager.User.add(
+            {
+                "name": "Elvira",
+                "surname": "Espindola",
+                "address": "Via del Corso, 22",
+                "phone": 30003,
+                "group": self.g_s
+            }
+        )
+
+        copies = doc_manager.Copy.select()
+        active_copies = [x for x in copies if x.active]
+        self.assertEqual(len(active_copies), 8)
+        users = user_manager.User.get_list(100, 1)
+        # Librarian is no a patron, only 1 librarian
+        self.assertEqual(len(users) + 1, 4)
+
+    def test_case_2(self):
+        self.test_case_1()
+        b1_c = self.b1.get_document_copies()
+        b1_ac = [x for x in b1_c if x.active]
+        doc_manager.Copy.remove(b1_ac[0].CopyID)
+        doc_manager.Copy.remove(b1_ac[1].CopyID)
+
+        b3_c = self.b1.get_document_copies()
+        b3_ac = [x for x in b3_c if x.active]
+        doc_manager.Copy.remove(b3_ac[0].CopyID)
+
+        user_manager.User.remove(self.p2.card_id)
+
+        copies = doc_manager.Copy.select()
+        active_copies = [x for x in copies if x.active]
+        self.assertEqual(len(active_copies), 5)
+        users = user_manager.User.get_list(100, 1)
+        # Librarian is no a patron, only 1 librarian
+        self.assertEqual(len(users) + 1, 3)
+
+    def test_case_3(self):
+        self.test_case_1()
+
+        p1 = user_manager.User.get_by_id(1000)
+        self.assertEqual(p1.name + " " + self.p1.surname, "Sergey Afonso")
+        self.assertEqual(p1.address, "Via Margutta, 3")
+        self.assertEqual(p1.phone, 30001)
+        self.assertEqual(p1.card_id, 1000)
+        self.assertEqual(p1.group, self.g_f)
+
+        p3 = user_manager.User.get_by_id(1002)
+        self.assertEqual(p3.name + " " + self.p3.surname, "Elvira Espindola")
+        self.assertEqual(p3.address, "Via del Corso, 22")
+        self.assertEqual(p3.phone, 30003)
+        self.assertEqual(p3.card_id, 1002)
+        self.assertEqual(p3.group, self.g_s)
+
+    def test_case_4(self):
+        self.test_case_2()
+
+        p3 = user_manager.User.get_by_id(1002)
+        self.assertEqual(p3.name + " " + self.p3.surname, "Elvira Espindola")
+        self.assertEqual(p3.address, "Via del Corso, 22")
+        self.assertEqual(p3.phone, 30003)
+        self.assertEqual(p3.card_id, 1002)
+        self.assertEqual(p3.group, self.g_s)
+
+        p2 = user_manager.User.get_by_id(1001)
+        self.assertEqual(p2.group.id, 1)
+
+    def test_case_5(self):
+        self.test_case_2()
+        bsystem = booking_system.Booking_system()
+        p2 = user_manager.User.get_by_id(1001)
+        res = bsystem.check_out(p2, self.b1c1, "Librarian")
+        self.assertEqual(res, (4, None))
+
+    def test_case_6(self):
+        """
+        self.test_case_2()
+        bsystem = booking_system.Booking_system()
+        p1 = user_manager.User.get_by_id(1001)
+        b1_not_checked_copies = [x for x in doc_manager.Book.get_document_copies(self.b1) if not x.checked_out]
+        bsystem.check_out(self.p1, b1_not_checked_copies[0], "Librarian")
+        b2_not_checked_copies = [x for x in doc_manager.Book.get_document_copies(self.b2) if not x.checked_out]
+        bsystem.check_out(self.p1, b2_not_checked_copies[0], "Librarian")
+        """
+        # Wrong test
+
+    def test_case_7(self):
+        self.test_case_1()
+        bsystem = booking_system.Booking_system()
+        p1 = user_manager.User.get_by_id(1001)
+        b1_not_checked_copies = [x for x in doc_manager.Book.get_document_copies(self.b1) if not x.checked_out]
+        b2_not_checked_copies = [x for x in doc_manager.Book.get_document_copies(self.b2) if not x.checked_out]
+        b3_not_checked_copies = [x for x in doc_manager.Book.get_document_copies(self.b3) if not x.checked_out]
+        av1_not_checked_copies = [x for x in doc_manager.Book.get_document_copies(self.av1) if not x.checked_out]
+        av2_not_checked_copies = [x for x in doc_manager.Book.get_document_copies(self.av2) if not x.checked_out]
+
+        res = bsystem.check_out(self.p1, b1_not_checked_copies[0], "Librarian")
+        self.assertEqual(res[0], 0)
+
+        res = bsystem.check_out(self.p1, b2_not_checked_copies[0], "Librarian")
+        self.assertEqual(res[0], 0)
+
+        # Cannot checkout: is reference
+        res = bsystem.check_out(self.p1, b3_not_checked_copies[0], "Librarian")
+        self.assertEqual(res[0], 2)
+
+        res = bsystem.check_out(self.p1, av1_not_checked_copies[0], "Librarian")
+        self.assertEqual(res[0], 0)
+
+        res = bsystem.check_out(self.p2, b1_not_checked_copies[1], "Librarian")
+        self.assertEqual(res[0], 0)
+
+        res = bsystem.check_out(self.p2, b2_not_checked_copies[1], "Librarian")
+        self.assertEqual(res[0], 0)
+        res = bsystem.check_out(self.p2, av2_not_checked_copies[0], "Librarian")
+        self.assertEqual(res[0], 0)
+
+        p1 = user_manager.User.get_by_id(1000)
+        self.assertEqual(p1.name + " " + self.p1.surname, "Sergey Afonso")
+        self.assertEqual(p1.address, "Via Margutta, 3")
+        self.assertEqual(p1.phone, 30001)
+        self.assertEqual(p1.card_id, 1000)
+        self.assertEqual(p1.group, self.g_f)
+
+        p1_checked_out = [p1_h.copy.get_doc() for p1_h in p1.operations]
+        p1_docs = [self.b1, self.b2, self.av1]
+        # documents which user should have
+        for p1_shd in p1_docs:
+            # documents which user actually have
+            have = False
+            for p1_d in p1_checked_out:
+                if type(p1_d) == type(p1_shd) and p1_d.DocumentID == p1_shd.DocumentID:
+                    self.assertFalse(have)
+                    have = True
+            self.assertTrue(have)
+
+        p1_operations = list(p1.operations)
+        for p1_h in p1_operations:
+            print(bsystem.get_max_return_time(p1_h))
+
+        p2 = user_manager.User.get_by_id(1001)
+        self.assertEqual(p2.name + " " + self.p2.surname, "Nadia Teixeira")
+        self.assertEqual(p2.address, "Via Sacra, 13")
+        self.assertEqual(p2.phone, 30002)
+        self.assertEqual(p2.card_id, 1001)
+        self.assertEqual(p2.group, self.g_s)
+
+        p2_checked_out = [p2_h.copy.get_doc() for p2_h in p2.operations]
+        p2_docs = [self.b1, self.b2, self.av2]
+        # documents which user should have
+        for p2_shd in p2_docs:
+            # documents which user actually have
+            have = False
+            for p2_d in p2_checked_out:
+                if type(p2_d) == type(p2_shd) and p2_d.DocumentID == p2_shd.DocumentID:
+                    self.assertFalse(have)
+                    have = True
+            self.assertTrue(have)
+
+    def test_case_8(self):
+        pass
+
+    def test_case_9(self):
+        pass
+
+
+
+
