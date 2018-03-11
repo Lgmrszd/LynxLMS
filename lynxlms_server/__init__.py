038beb1f064af82e3e2bb7b0cbd9e684ec92f137
Mikhail <m.v.bobrov@yandex.ru>
2018-03-11 17:22:42 +0300

    add module in __init__

--- lynxlms_server/__init__.py
+++ lynxlms_server/__init__.py
@@ -1,4 +1,5 @@
 from .lmsdb import BaseModel
-from .managers import Document, DocType, Group, User, Librarian, Copy, Entry
+from .managers import Document, DocType, Group, User, Copy
+from .booking_system import Librarian, Entry, Session
 
-__all__ = ["config", "BaseModel", "Document", "DocType", "Group", "User", "Librarian", "Copy", "Entry"]
+__all__ = ["config", "BaseModel", "Document", "DocType", "Group", "User", "Librarian", "Copy", "Entry", "Session"]
