c28cfb230b60540f72c792be9c97376c5f568985
PolarNorth <antonov9312@yandex.ru>
2018-03-31 21:52:08 +0300

    Renew time added

--- managers/group_manager.py
+++ managers/group_manager.py
@@ -1,4 +1,5 @@
 from peewee import *
+from playhouse.migrate import *
 from db_connect import BaseModel
 from managers.doc_manager import class_to_name
 
@@ -10,12 +11,20 @@ class Group(BaseModel):
     book_bestseller_ct = SmallIntegerField()
     journal_ct = SmallIntegerField()
     av_ct = SmallIntegerField()
+    book_rt = SmallIntegerField()
+    book_bestseller_rt = SmallIntegerField()
+    journal_rt = SmallIntegerField()
+    av_rt = SmallIntegerField()
     priority = SmallIntegerField()
     fields = {"name": name,
               "book_ct": book_ct,
               "book_bestseller_ct": book_bestseller_ct,
               "journal_ct": journal_ct,
               "av_ct": av_ct,
+              "book_rt": book_ct,
+              "book_bestseller_rt": book_bestseller_ct,
+              "journal_rt": journal_ct,
+              "av_rt": av_ct,
               "priority": priority}
 
     @classmethod
@@ -42,12 +51,25 @@ class Group(BaseModel):
 
     def get_checkout_time(self, doc):
         doc_type = class_to_name()[type(doc)]
-        if doc_type == "Book":
+        if doc_type == "Book" and not ('best seller' in doc.keywords):
             return self.book_ct
+        elif doc_type == "Book" and 'best seller' in doc.keywords:
+            return self.book_bestseller_ct
         elif doc_type == "JournalArticle":
             return self.journal_ct
         elif doc_type == "AVMaterial":
             return self.av_ct
+    
+    def get_renew_time(self, doc):
+        doc_type = class_to_name()[type(doc)]
+        if doc_type == "Book" and not ('best seller' in doc.keywords):
+            return self.book_rt
+        elif doc_type == "Book" and 'best seller' in doc.keywords:
+            return self.book_bestseller_rt
+        elif doc_type == "JournalArticle":
+            return self.journal_rt
+        elif doc_type == "AVMaterial":
+            return self.av_rt
 
     @classmethod
     def get_list(cls, rows_number, page):
