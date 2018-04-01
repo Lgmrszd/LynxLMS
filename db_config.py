d2c8f759205981482a7ff656eefcadc0df06b7cd
PolarNorth <antonov9312@yandex.ru>
2018-04-01 11:41:12 +0300

    create/drop tables and deleted group fixed

--- db_config.py
+++ db_config.py
@@ -24,11 +24,13 @@ def create_tables():
                       managers.user_manager.User,
                       managers.group_manager.Group,
                       managers.booking_system.History,
-                      managers.booking_system.Queue], safe=True)
+                      managers.user_manager.Queue,
+                      managers.user_manager.Request], safe=True)
     #deleted_group = managers.group_manager.Group.get(managers.group_manager.Group.name == 'Deleted')
     deleted_group = len(managers.group_manager.Group.select().where(managers.group_manager.Group.name == 'Deleted'))
     if (deleted_group == 0):
-        managers.group_manager.Group.create(name='Deleted', book_ct=-1, book_bestseller_ct=-1, journal_ct=-1, av_ct=-1, priority=-1)
+        managers.group_manager.Group.create(name='Deleted', book_ct=-1, book_bestseller_ct=-1, journal_ct=-1, av_ct=-1,
+                                            book_rt=-1, book_bestseller_rt=-1, journal_rt=-1, av_rt=-1, priority=-1)
 
 
 def drop_tables():
@@ -38,5 +40,7 @@ def drop_tables():
                     managers.doc_manager.Copy,
                     managers.user_manager.User,
                     managers.group_manager.Group,
-                    managers.booking_system.History], safe=True)
+                    managers.booking_system.History,
+                    managers.user_manager.Queue,
+                    managers.user_manager.Request], safe=True)
 
