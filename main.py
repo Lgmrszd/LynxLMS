2931899ce2886651f251957e369dcca3bedf4bb2
Mikhail <m.v.bobrov@yandex.ru>
2018-04-02 13:47:42 +0300

    something suggested by Anton

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from gui.App import App
 import db_config
+import managers.user_manager
 
 if __name__ == "__main__":
     db_config.init_db()
+    managers.user_manager.Queue.update_queue()
     app = App()#starts application cycle
