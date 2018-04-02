1e658e286246a1f79417b9e35261b230f5e307ef
Mikhail <m.v.bobrov@yandex.ru>
2018-04-02 13:45:08 +0300

    fix

--- db_config.py
+++ db_config.py
@@ -32,6 +32,21 @@ def create_tables():
         managers.group_manager.Group.create(name='Deleted', book_ct=-1, book_bestseller_ct=-1, journal_ct=-1, av_ct=-1,
                                             book_rt=-1, book_bestseller_rt=-1, journal_rt=-1, av_rt=-1, priority=-1)
 
+    students_group = len(managers.group_manager.Group.select().where(managers.group_manager.Group.name == 'Students'))
+    if (students_group == 0):
+        managers.group_manager.Group.create(name='Students', book_ct=3, book_bestseller_ct=1, journal_ct=2, av_ct=2,
+                                        book_rt=3, book_bestseller_rt=1, journal_rt=2, av_rt=2, priority=1)
+
+    faculty_group = len(managers.group_manager.Group.select().where(managers.group_manager.Group.name == 'Faculty'))
+    if (faculty_group == 0):
+        managers.group_manager.Group.create(name='Faculty', book_ct=4, book_bestseller_ct=4, journal_ct=2, av_ct=2,
+                                        book_rt=4, book_bestseller_rt=4, journal_rt=2, av_rt=2, priority=2)
+
+    vp_group = len(managers.group_manager.Group.select().where(managers.group_manager.Group.name == 'Visiting professors'))
+    if (vp_group == 0):
+        managers.group_manager.Group.create(name='Visiting professors', book_ct=4, book_bestseller_ct=4, journal_ct=2, av_ct=2,
+                                        book_rt=1, book_bestseller_rt=1, journal_rt=1, av_rt=1, priority=0)
+
 
 def drop_tables():
     db.drop_tables([managers.doc_manager.Book,
