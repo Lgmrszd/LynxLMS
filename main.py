7ecb9d881720548d7873e3250cbcc8e60e723776
PolarNorth <antonov9312@yandex.ru>
2018-04-22 21:12:03 +0300

    Logging threshhold is INFO now

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import managers.user_manager
 import logging
 
 if __name__ == "__main__":
-    logging.basicConfig(filename='main.log', level=logging.DEBUG, format='%(asctime)s \
-                        | %(levelname)s: %(message)s ') #logging confgi description
+    logging.basicConfig(filename='main.log', level=logging.INFO, format='%(asctime)s \
+                        | %(levelname)s: %(message)s ') #logging config description
     db_config.init_db()
     managers.user_manager.Queue.update_queue()
     app = App()#starts application cycle
