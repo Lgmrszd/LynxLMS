6967e65c8340cb90a23ca3165e02adbd3a8315e1
Mikhail <m.v.bobrov@yandex.ru>
2018-04-23 02:24:52 +0300

    (comment old function)

--- managers/notifier.py
+++ managers/notifier.py
@@ -12,21 +12,21 @@ __password = __email_settings["pass"]
 DEBUG = True
 
 
-def send_message(email, subj, body):
-    msg = MIMEMultipart()
-    msg['From'] = __address
-    msg['To'] = email
-    msg['Subject'] = subj
-    msg.attach(MIMEText(body, 'plain'))
-
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(__address, __password)
-    # TODO: fast
-    # TODO: after checkout
-    text = msg.as_string()
-    server.sendmail(__address, email, text)
-    server.quit()
+# def send_message(email, subj, body):
+#     msg = MIMEMultipart()
+#     msg['From'] = __address
+#     msg['To'] = email
+#     msg['Subject'] = subj
+#     msg.attach(MIMEText(body, 'plain'))
+#
+#     server = smtplib.SMTP('smtp.gmail.com', 587)
+#     server.starttls()
+#     server.login(__address, __password)
+#     # TODO: fast
+#     # TODO: after checkout
+#     text = msg.as_string()
+#     server.sendmail(__address, email, text)
+#     server.quit()
 
 
 def __send_messages_task_func(task_id, args):
