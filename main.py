59948c9ea3617b57fc4ea4226b5814a95bd7e71d
PolarNorth <antonov9312@yandex.ru>
2018-04-22 23:50:30 +0300

    Intendation in the logging fixed

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import logging
 
 if __name__ == "__main__":
     logging.basicConfig(filename='main.log', level=logging.INFO, format='%(asctime)s \
-                        | %(levelname)s: %(message)s ') #logging config description
+    | %(levelname)s: %(message)s ') #logging config description
     db_config.init_db()
     managers.user_manager.Queue.update_queue()
     app = App()#starts application cycle
