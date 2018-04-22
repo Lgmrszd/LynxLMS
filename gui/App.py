32e178455bed20631500625f738e3129299e2746
Gayaz Kamaletdinov <gazikkamalov2@gmail.com>
2018-04-22 16:01:30 +0300

    auth window
    asasdasdasdasd

--- gui/App.py
+++ gui/App.py
@@ -1,13 +1,14 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QStyleFactory
 from gui.MainWindow import MainWindow
+from gui.Authorisation import Authorization
 
 class App:
     def __init__(self):
         app = QApplication(sys.argv)
         QApplication.setStyle(QStyleFactory.create('Fusion'))
 
-        main_window = MainWindow()
-        main_window.show()
+        auth = Authorization()
+        auth.show()
 
         sys.exit(app.exec_())#will not end until you close the program
---
+++ gui/Authorisation.py
@@ -0,0 +1,68 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QDesktopWidget, QPushButton, QVBoxLayout, QLineEdit, QHBoxLayout, QLabel
+from managers.auth import Auth
+from gui.MainWindow import MainWindow
+
+class Authorization(QWidget):
+    def __init__(self):
+        super().__init__()
+        self._set_up_ui()
+
+    def _set_up_ui(self):
+        window_size_x = 400
+        window_size_y = 300
+
+        hbox = QHBoxLayout()
+        hbox.addStretch()
+
+        self.login_label = QLabel("Login")
+        self.pass_label = QLabel("Password")
+
+        self.login_edit = QLineEdit("")
+        self.pass_edit = QLineEdit("")
+
+        self.status = QLabel("")
+
+        self.login_button = QPushButton("login")
+        self.login_button.setFixedHeight(30)
+        self.login_button.clicked.connect(self.login)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch()
+        vbox.addWidget(self.login_label)
+        vbox.addWidget(self.login_edit)
+        vbox.addWidget(self.pass_label)
+        vbox.addWidget(self.pass_edit)
+        vbox.addWidget(self.login_button)
+        vbox.addWidget(self.status)
+        vbox.addStretch()
+
+        hbox.addLayout(vbox)
+
+        hbox.addStretch()
+
+        self.setLayout(hbox)
+        self.setFixedSize(window_size_x, window_size_y)
+        self._center()
+        self.setWindowTitle('Authorization')
+
+    def _center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def login(self):
+        login = self.login_edit.text()
+        password = self.pass_edit.text()
+        if len(login) == 0:
+            self.status.setText("<font color='red'> Empty login field </font>")
+            return
+        if len(password) == 0:
+            self.status.setText("<font color='red'> Empty password field </font>")
+            return
+        print(login + " " + password)
+        Auth.login(login, password)
+        self.main_window = MainWindow()
+        self.main_window.show()
+        self.hide()
