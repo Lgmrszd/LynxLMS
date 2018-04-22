e78ec36c242744e71b9e0ab10c8644fdc327dbe5
Mikhail <m.v.bobrov@yandex.ru>
2018-04-23 02:21:40 +0300

    new notifier functions

--- managers/notifier.py
+++ managers/notifier.py
@@ -95,3 +95,28 @@ def notify_free_copy(users, doc):
         user_info = {"email": user.email, "name": f"{user.name} {user.surname}"}
         users_info.append(user_info)
     __send_messages(display_name, users_info, subj, body)
+
+
+def notify_request_overdue(users, doc):
+    doc_name = doc.title
+    display_name = "Notify users about request removing"
+    subj = "Removed from queue"
+    body = "Dear {},\nYour request for document \"%s\" is overdue and has been removed" % doc_name
+    users_info = []
+    for user in users:
+        user_info = {"email": user.email, "name": f"{user.name} {user.surname}"}
+        users_info.append(user_info)
+    __send_messages(display_name, users_info, subj, body)
+
+
+def notify_doc_abandon(users, doc):
+    doc_name = doc.title
+    display_name = "Notify users about free copies"
+    subj = "Document queue abandoned"
+    body = "Dear {},\nQueue for the document \"%s\" have been abandoned due to outstanding request.\n" % doc_name
+    users_info = []
+    for user in users:
+        user_info = {"email": user.email, "name": f"{user.name} {user.surname}"}
+        users_info.append(user_info)
+    __send_messages(display_name, users_info, subj, body)
+
