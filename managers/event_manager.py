afbc41a5e52747ce1de054c22e3cc6b8c1305f64
Mikhail <m.v.bobrov@yandex.ru>
2018-04-23 13:19:36 +0300

    Documentation for event_manager

--- managers/event_manager.py
+++ managers/event_manager.py
@@ -1,7 +1,14 @@
+"""List of all listeners"""
 _listeners = {}
 
 
 def register_listener(name, func):
+    """
+    Register listener in event manager
+    :param name: name of the listener
+    :param func: function corresponding to listener
+    :return:
+    """
     listeners = _listeners.get(name, None)
     if listeners:
         _listeners[name].append(func)
@@ -10,6 +17,12 @@ def register_listener(name, func):
 
 
 def send_event(name, *args):
+    """
+    Send event to registered listeners
+    :param name: name of the listener
+    :param args: arguments for the listener, if any
+    :return:
+    """
     listeners = _listeners.get(name, None)
     if listeners:
         if args:
